8373bd5cc56b27dd800c7237661c506fa5fd2255
Noah Gibson <noah.gibson99@gmail.com>
2020-02-18 16:17:30 -0700

    Add cors

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const fetch = require('node-fetch');
 const redis = require('redis');
+const cors = require('cors');
 
 const PORT = process.env.PORT || 3001;
 const REDIS_PORT = process.env.PORT || 6379;
@@ -8,6 +9,7 @@ const REDIS_PORT = process.env.PORT || 6379;
 const client = redis.createClient(REDIS_PORT);
 
 const app = express();
+app.use(cors());
 
 app.get('/repositories/:keyword', cacheMiddleware, async (req, res) => {
   try {
